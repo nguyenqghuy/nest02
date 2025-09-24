fa191c2872bb44945e052d6147a3d8fd61752fda
nguyen quang huy <nguyenqghuy@gmail.com>
2025-09-24 14:55:18 +0700

    --no-edit

--- src/common/auth.guard.ts
+++ src/common/auth.guard.ts
@@ -5,7 +5,7 @@ export class AuthGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean {
         const request = context.switchToHttp().getRequest();
         console.log('AuthGuard running, x-auth:', request.headers['x-auth']);
-        if (request.headers['x-auth'] !== 'secret') {
+        if (request.headers['x-auth'] !== 'huy') {
             throw new UnauthorizedException('Missing or invalid x-auth header');
         }
         return true;
